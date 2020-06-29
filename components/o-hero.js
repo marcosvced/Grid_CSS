0affc5ec921abc9b991a2cc4f6641e9da3c13acf
Marcos <devler.mve@gmail.com>
2020-06-29 14:12:19 +0200

    feat: hero introduction

--- components/o-hero.js
+++ components/o-hero.js
@@ -2,10 +2,23 @@ const OHero = Vue.component('o-hero', {
     template: `
      <section class="o-hero -mb-5">
         <div class="m-hero__wrapper -container">
-            <h1 class="a-hero__title">
+        <div class="-display-row -wrap -justify-between -align-start">
+            <h1 class="a-hero__title -col-12 -lg-col-8">
                 Aguia<br/> Design System
             </h1>
-            <a class="a-button -btn-link" href="https://github.com/marcosvced/Grid_CSS" target="_blank">Download <i class="a-icon -arrow-right"></i> </a>
+            <div class="m-hero__content -col-12 -md-col-7 -lg-col-4">
+                <header class="m-header -col-12 -mb-2">
+                    <p class="a-header__abstract">INTRODUCTION</p>
+                    <h2 class="a-header__tile">A simple responsive framework</h2>
+                    <p class="a-header__description">
+                        that offers a starting point with which you don't need to fight with the unnecessary default styles of other frameworks.
+                        With which you can organize your code using the ABEM methodology that you can customize as never before.  
+                    </p>
+                </header>
+               <a class="a-button -btn-link" href="https://github.com/marcosvced/Grid_CSS" target="_blank">Download <i class="a-icon -arrow-right"></i> </a>
+            </div>
+        </div>
+
         </div>
     </section>
     `
