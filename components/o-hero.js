3bc634062d391f41a2ad226375b22bc723f6bdd1
Marcos <devler.mve@gmail.com>
2020-07-01 13:08:41 +0200

    fix: new aguia-css version

--- components/o-hero.js
+++ components/o-hero.js
@@ -51,7 +51,7 @@ const OHero = Vue.component('o-hero', {
             const url = window.URL.createObjectURL(new Blob([response.data]))
             const link = document.createElement('a')
             link.href = url
-            link.setAttribute('download', 'aguia@1.2.0.zip') //or any other extension
+            link.setAttribute('download', 'aguia@1.3.0.zip') //or any other extension
             document.body.appendChild(link)
             link.click()
         },
